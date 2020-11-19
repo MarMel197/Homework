36c81457d327d4b8437ccf84faaef698a63499ef
Mark <markmeldrum70@gmail.com>
2020-11-19 10:09:00 +0000

    fixed

--- homework_W6_d3/enumeration_traveller_hw/hw_enumeration_traveller_start/models/traveller.js
+++ homework_W6_d3/enumeration_traveller_hw/hw_enumeration_traveller_start/models/traveller.js
@@ -21,22 +21,29 @@ Traveller.prototype.getJourneysByTransport = function (transport) {
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-    return this.journeys.every((journey) => {
-      return journey.distance >= minDistance;
+    return this.journeys.filter((journey) => {
+      return journey.distance > minDistance;
     });
 };
-
+// Zero at end puts in start value for reduce
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
   return this.journeys.reduce((total, journey) => {
-    return total += journey.distance;
+    return total + journey.distance;
   }, 0);
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
   return this.journeys.map((journey) => {
-    return new Set(journey.transport);
+    return journey.transport;
   })
+  .filter((transport, index, array) => {
+    return array.indexOf(transport) === index;
+  });
 };
+  // return this.journeys.map((journey) => {
+  //   return new Set(journey.transport);
+//   })
+// };
 
 
 module.exports = Traveller;
